ec2d5f1c4d0f0fe2925492717506189573bd4ddd
marcelo lima do nascimento <30661697+marcklima@users.noreply.github.com>
2022-04-05 11:35:25 -0300

    calc

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var mosrtrarSite = true;
-const  site = "www.google.com.br";
-console.log("hello");
-console.log("aprendendo js");
-if(mosrtrarSite){
-    console.log("Navegue no site: " + site)
+function soma (a,b){
+    return a+b;
 }
+
+function sub(a,b){
+    return a-b;
+
+}
+
+function mult(a,b){
+return  a*b;
+}
+
+function div(a,b){
+return a/b ;
+}
+
+console.log("A soma é: "+ soma(10, 30));
+console.log("o resultado mix é: " + div(mult(sub(200,30),40), soma(50,70)
+)//função div
+)//sonole.log;
