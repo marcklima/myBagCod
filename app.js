f0c60b3d3db7d640b836a6cab39c2beffcb95b80
marcelo lima do nascimento <30661697+marcklima@users.noreply.github.com>
2022-04-05 13:10:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const soma = require("./calc"); //CHAMANDO O module.exports = soma em calc.js
+const valores = require("./cconstantes");//chamando as constantes 
 
-console.log(" A SOMA É : " + soma(12,12));// imprimindo a função soma em cal.js
+console.log(" A SOMA É : " + soma.soma(valores.anoAtual, valores.impar));// imprimindo a função soma em cal.js
